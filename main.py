fadc60a7f86083ab3f292d3182a3c02a5c422666
yuliasimha <yulia.simha@gmail.com>
2021-02-08 22:30:16 +0200

    Added serverless

--- main.py
+++ main.py
@@ -4,21 +4,25 @@ import time
 import requests
 
 auditData = {
-  "time": 3,
-  "username": "arnold",
-  "message": "yulia connection succeeded",
+  "time": 32345,
+  "username": "user5",
+  "message": "user5 connection succeeded",
   "action": "connection"
 }
 
-auditsMgr = auditsManager.auditsManager()
+#auditsMgr = auditsManager.auditsManager()
 #auditsMgr.writeAudit(auditData)
+
 
 #auditsMgr.readAudit("yulia", 6, 123456128)
 
 def send():
-  url = "http://127.0.0.1:5000/api/audits/"
-  data = {"time": 712, "username": "arnold3", "message": "yulia connection succeeded","action": "connection"}
+  url = "https://i5o7w3hula.execute-api.eu-west-3.amazonaws.com/dev/api/audits/"
+  data = {"time": 712, "username": "user8", "message": "user8 connection succeeded","action": "connection"}
   headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-  requests.post(url, data=json.dumps(data), headers=headers)
-  
-send()    
+  response = requests.post(url, data=json.dumps(data), headers=headers)
+  print(response)
+
+send() 
+
+v= 12   
